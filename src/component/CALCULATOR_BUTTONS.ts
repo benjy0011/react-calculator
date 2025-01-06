89b7eb688dc2c9c3336a99a40ab70b32c96a8bf7
Benjy <benjyyeoh.wx@ynytechnology.com.my>
2025-01-06 14:28:03 +0800

    first commit

---
+++ src/component/CALCULATOR_BUTTONS.ts
@@ -0,0 +1,9 @@
+const CALCULATOR_BUTTONS = [
+    {display: "(", arimetic: "(",},
+    {display: ")", arimetic: ")",},
+    {display: "%", arimetic: "* 0.01"},
+    {display: "CE", arimetic: "clear"},
+    
+];
+
+export default CALCULATOR_BUTTONS;
